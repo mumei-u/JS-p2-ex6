609000bc313fc795d77a14b17aefb015134c77a6
thyago_lbt <thyago14@outlook.fr>
2019-10-25 09:48:44 +0200

    fin

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+function img1() {
+  document.getElementById('image1').src="assets/img/image1.jpg";
+}
+function img1_2() {
+  document.getElementById('image1').src="assets/img/image1_2.jpg";
+}
+function img2() {
+  document.getElementById('image2').src="assets/img/image2.jpg";
+}
+function img2_2() {
+  document.getElementById('image2').src="assets/img/image2_2.jpg";
+}
+function img3() {
+  document.getElementById('image3').src="assets/img/image3.jpg";
+}
+function img3_2() {
+  document.getElementById('image3').src="assets/img/image3_2.jpg";
+}
+function img4() {
+  document.getElementById('image4').src="assets/img/image4.jpg";
+}
+function img4_2() {
+  document.getElementById('image4').src="assets/img/image4_2.jpg";
+}
+function img5() {
+  document.getElementById('image5').src="assets/img/image5.jpg";
+}
+function img5_2() {
+  document.getElementById('image5').src="assets/img/image5_2.jpg";
+}
